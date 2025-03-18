243f9ef3ad42c75e64e9ad7555448552a59a45a0
KyrillWil <kyrill.willems@student.tsaam.be>
2025-03-18 12:02:45 +0100

    push1

--- script/script.js
+++ script/script.js
@@ -5,3 +5,32 @@ const gebruikers = [
   { id: 4, login: "jos.teugels", leeftijd: 13 },
   { id: 5, login: "piet.pollekes", leeftijd: 27 },
 ];
+//
+const loginGegevens = document.querySelector("#login")
+const leeftijd = document.querySelector("#leeftijd")
+const toevoegen = document.querySelector("#toevoegen")
+const jonger = document.querySelector("#jonger")
+const ouder = document.querySelector("#ouder")
+const filterLeeftijd = document.querySelector("#filterLeeftijd")
+const lijstNamen = document.querySelector("#lijstNamen")
+
+//toevoegen knop
+toevoegen.addEventListener("click", () =>{
+  console.log(loginGegevens.value);
+  gebruikers.push(loginGegevens)
+  console.log(gebruikers);
+  
+})
+console.log(gebruikers[1].login);
+//ouder jonger knop
+jonger.addEventListener("click",()=>{
+  for (let i = 0; i < gebruikers.length; i++) {
+    if (filterLeeftijd.value > gebruikers[i].leeftijd) {
+    //console.log(filterLeeftijd[i].login);
+  }
+  }
+})
+  lijstNamen.textContent = "hoi"
+
+
+
